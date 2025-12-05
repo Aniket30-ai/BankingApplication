7987d6f86d325cec429fd8e01124d9749c3d0464
Aniket30-ai <aniketdhakate30@gmail.com>
2025-12-05 12:39:58 +0530

    Update BankAccountService.java
    Update to trigger

--- src/main/java/com/nihilent/bank/service/BankAccountService.java
+++ src/main/java/com/nihilent/bank/service/BankAccountService.java
@@ -10,7 +10,7 @@ import com.nihilent.bank.exception.NihilentBankException;
 
 public interface BankAccountService {
 
-	// update to trigger CI pipeline
+	// update to trigger CI pipeline in azure
 	public Long createAccount(BankAccountDto accountDto) throws NihilentBankException;
 
 	public BankAccountDto getAccountDetails(Long accountNumber) throws NihilentBankException;
